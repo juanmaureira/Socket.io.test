9b220183b460ce50236a2cdb7dcdacd8f32fc5ee
DecimalServices2020 <juanmaureira3@gmail.com>
2022-05-31 20:46:32 -0400

    Socket.io
    initialize socker in port 4000

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const userRoutes = require('./routers/userRoutes')
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const userRoutes = require("./routers/userRoutes");
+const { Server } = require("socket.io");
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }))
+const io = new Server(4000, {
+  // options
+});
 
-app.use('/', userRoutes)
+io.on("connection", (socket) => console.log("Socket iniciado ..."));
+io.emit("event", () => console.log("evento"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", userRoutes);
 
-module.exports = app
+module.exports = app;
--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,11 @@
-const { save } = require('../models')
+const { save } = require("../models");
 
 const setUser = (req, res) => {
-  console.log('user -> ', req.body)
-  save()
-  return res.status(200).send({})
-}
+  console.log("user -> ", req.body);
+  save();
+  return res.status(200).send({});
+};
 
 module.exports = {
   setUser,
-  saveUser
-}
+};
